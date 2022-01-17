ff833b98871a8be63adca8c17eb1ba926a1c133f
youmari <youmari300@gmail.com>
2022-01-17 22:50:27 +0100

    Add React-router to index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
+
+// import { Provider } from 'react-redux'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    {/* <Provider store={}> */}
+    <Router>
+      <App />
+    </Router>
+    {/* </Provider> */}
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
